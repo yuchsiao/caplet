0e3d54a1635966bfbb4689b98c8ff937b4951239
Yu-Chung Hsiao <yuchsiao@gmail.com>
2013-07-12 02:49:22 -0400

    Update caplet_gds2geo/install.sh by adding wget option

--- caplet_gds2geo/install.sh
+++ caplet_gds2geo/install.sh
@@ -5,7 +5,7 @@ export GDSII_EXT=.tar.gz
 
 echo "caplet_gds2geo: Clean up gdsii folder and fetch python-gdsii ..."
 rm -r gdsii
-wget "http://pypi.python.org/packages/source/p/python-gdsii/$GDSII$GDSII_EXT"
+wget --no-check-certificate "https://pypi.python.org/packages/source/p/python-gdsii/$GDSII$GDSII_EXT"
 echo "caplet_gds2geo: Decompress python-gdsii ..."
 tar -xf $GDSII$GDSII_EXT
 echo "caplet_gds2geo: Move gdsii folder and delete downloaded files ..."
